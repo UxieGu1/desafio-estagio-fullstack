a5e20de87fbb13f3c9c66ec24e01c782e1c6100e
Guilherme Freires <guilhermefreires10@gmail.com>
2026-03-20 17:22:33 -0300

    Comando SQL para criação da tabela de candidaturas

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -7,3 +7,16 @@ CREATE TABLE IF NOT EXISTS tb_vagas{
     status VARCHAR(50) NOT NULL DEFAULT 'aberta' -- ex: aberta e fechada
 };
 
+CREATE TABLE IF NOT EXISTS candidaturas (
+    id BIGSERIAL PRIMARY KEY,
+    vaga_id BIGINT NOT NULL,
+    nome_candidato VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'em_analise', -- ex: em_analise, aprovado, reprovado
+
+--relacionamento 1:N
+    CONSTRAINT fk_vaga
+    FOREIGN KEY (vaga_id)
+    REFERENCES vagas(id)
+    ON DELETE RESTRICT
+);
