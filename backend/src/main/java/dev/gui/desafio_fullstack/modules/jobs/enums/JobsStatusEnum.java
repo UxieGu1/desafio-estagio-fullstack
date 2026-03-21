6eb068c6ec62cacb5165935670d6b566179462d5
Guilherme Freires <guilhermefreires10@gmail.com>
2026-03-20 22:11:52 -0300

    Criação dos enums

---
+++ backend/src/main/java/dev/gui/desafio_fullstack/modules/jobs/enums/JobsStatusEnum.java
@@ -0,0 +1,6 @@
+package dev.gui.desafio_fullstack.modules.jobs.enums;
+
+public enum JobsStatusEnum {
+    OPEN,
+    CLOSED
+}
---
+++ backend/src/main/java/dev/gui/desafio_fullstack/modules/jobs/enums/TypeEnum.java
@@ -0,0 +1,7 @@
+package dev.gui.desafio_fullstack.modules.jobs.enums;
+
+public enum TypeEnum {
+    INTERNSHIP, //estagio
+    JUNIOR,
+    MID_LEVEL //pleno
+}
