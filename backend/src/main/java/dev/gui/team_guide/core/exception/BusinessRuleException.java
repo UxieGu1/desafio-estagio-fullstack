0212ee7ac257ea08d6119e306bd6fa83256e5445
Guilherme Freires <guilhermefreires10@gmail.com>
2026-03-23 10:09:33 -0300

    Criação das classes de tratamento de exceções

---
+++ backend/src/main/java/dev/gui/team_guide/core/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package dev.gui.team_guide.core.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/dev/gui/team_guide/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package dev.gui.team_guide.core.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
